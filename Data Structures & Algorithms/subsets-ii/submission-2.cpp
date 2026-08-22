5537c83c979bcada39c50713bcc71120af938b89
Mohil Sharma <165997372+mohil-sharma@users.noreply.github.com>
2026-08-22 11:02:09 +0530

    Add: subsets-ii - submission-2

---
+++ Data Structures & Algorithms/subsets-ii/submission-2.cpp
@@ -0,0 +1,38 @@
+class Solution {
+   public:
+    void helper(vector<int>& nums, int i, vector<int>& temp, vector<vector<int>>& ans,
+                set<vector<int>>& v) {
+        if (i == nums.size()) {
+            if (v.find(temp) == v.end()) {
+                ans.push_back(temp);
+                v.insert(temp);
+            }
+            return;
+        }
+
+        temp.push_back(nums[i]);
+
+        if (v.find(temp) == v.end()) {
+            ans.push_back(temp);
+            v.insert(temp);
+        }
+
+        helper(nums, i + 1, temp, ans, v);
+        temp.pop_back();
+        int j=i;
+        while(j<nums.size() && nums[j]==nums[i]){
+            j++;
+        }
+        helper(nums, j, temp, ans, v);
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<int> temp;
+        vector<vector<int>> ans;
+        set<vector<int>> v;
+
+        sort(nums.begin(), nums.end());
+        helper(nums, 0, temp, ans, v);
+
+        return ans;
+    }
+};
